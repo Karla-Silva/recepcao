4850309b454af580488f8acd4e23a4e271893263
Karla-Silva <mkarla@outlook.com>
2023-03-06 19:14:59 -0300

    Initial commit

---
+++ src/main/java/com/estacionamento/recepcao/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.estacionamento.recepcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/estacionamento/recepcao/repositories/MensalistaRepository.java
@@ -0,0 +1,6 @@
+package com.estacionamento.recepcao.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MensalistaRepository extends JpaRepository {
+}
