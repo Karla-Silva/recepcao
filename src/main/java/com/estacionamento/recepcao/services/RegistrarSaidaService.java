1784267beed6af4dc096e8bef93ba498f96a8c82
Fernanda Savatin <104535801+fernandasav@users.noreply.github.com>
2023-03-07 23:38:05 -0300

    Update RegistrarSaidaService.java
    add savedPedido

--- src/main/java/com/estacionamento/recepcao/services/RegistrarSaidaService.java
+++ src/main/java/com/estacionamento/recepcao/services/RegistrarSaidaService.java
@@ -11,10 +11,17 @@ public class RegistrarSaidaService {
     private final VagasRepository vagasRepository;
 
     public String execute(SaidaRequest saidaRequest){
-
-        //financeiro informar valor
-
+        
+        Pedido savedPedido= pedidoRepository.save(pedido);
+        itemPedidos
+                .forEach(itemPedido ->
+                        itemPedido.setItemPedidoComposedKey
+                                (new ItemPedidoComposedKey(savedPedido.getId(),itemPedido.getVaga().getId())));
+        itemPedidoRepository.saveAll(itemPedidos);
+        savedPedido.setItemPedidoList(itemPedidos);
         vagasRepository.deleteByPlaca(saidaRequest.getPlaca());
+        
         return "Carro devolvido";
+       
     }
 }
